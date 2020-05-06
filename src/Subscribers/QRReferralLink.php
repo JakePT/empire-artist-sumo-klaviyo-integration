7653ca84efe7a46dcf982dddfcffaa3f47b7f4eb
Jacob Peattie <jakept@me.com>
2020-05-06 20:26:34 +1000

    Add caching headers to QR code image.

--- src/Subscribers/QRReferralLink.php
+++ src/Subscribers/QRReferralLink.php
@@ -109,7 +109,12 @@ class QRReferralLink implements HasActions, HasFilters {
 			return;
 		}
 
+		header( 'Pragma: public' );
+		header( 'Cache-Control: max-age=86400' );
+		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', time() + 86400 ) );
+
 		QRcode::png( $url, false, QR_ECLEVEL_L, 16 );
+		
 		exit;
 	}
 
